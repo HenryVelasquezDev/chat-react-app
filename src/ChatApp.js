0114c7554d722d8603be37122dd627a4d6c14806
henry.velasquez <henryalex25@gmail.com>
2021-08-19 11:38:12 -0500

    se configuran las rutas principales de la app con react-router

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const ChatApp = () => {
     return (
         <div>
-            chat app
+            <AppRouter/>
         </div>
     )
 }
