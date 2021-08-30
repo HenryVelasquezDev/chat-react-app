09211d8f4abecb7dbb4398015bced266c3a4c58f
henry.velasquez <henryalex25@gmail.com>
2021-08-30 10:51:45 -0500

    finalizando front end de chatt app

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import { SideBarChatItem } from './SideBarChatItem'
+import React, { useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
+import { ChatContext } from '../context/chat/ChatContext';
+import { SideBarChatItem } from './SideBarChatItem';
 
 export const SideBar = () => {
 
-    const chats = [1,2,3,4,5,6,7,8,9,10];
+    const { chatState } = useContext(ChatContext);
+    const { auth } = useContext(AuthContext);
 
     return (
         <div className="inbox_chat">
             {
-                chats.map( (chat) =>(
-                    <SideBarChatItem key={ chat } />
+                chatState.usuarios
+                .filter( (usuario) => ( usuario.uid !== auth.uid ) )
+                .map((usuario) => (
+                    <SideBarChatItem
+                        key={usuario.uid}
+                        usuario = { usuario }
+                    />
                 ))
             }
 
---
+++ src/context/chat/chatReducer.js
@@ -0,0 +1,58 @@
+import { types } from "../../types/types";
+/*
+const initialState = {
+    uid: '',
+    chatActivo: null, //UID del usuario al que yo quiero enviar mensaje
+    usuarios: [], //todos los usuarios de la bd
+    mensajes: [],   //El chat seleccionado
+}
+*/
+
+export const chatReducer = (state, action) => {
+
+    switch (action.type) {
+
+        case types.usuariosCargados:
+            return {
+                ...state,
+                usuarios: [...action.payload]
+            }
+
+        case types.activarChat:
+            if (state.chatActivo === action.payload) return state;
+            return {
+                ...state,
+                chatActivo: action.payload,
+                mensajes: []
+            }
+
+        case types.nuevoMensaje:
+            if (state.chatActivo === action.payload.de ||
+                state.chatActivo === action.payload.para) {
+                return {
+                    ...state,
+                    mensajes: [...state.mensajes, action.payload]
+                }
+            } else {
+                return state;
+            }
+
+        case types.cargarMensajes:
+            return {
+                ...state,
+                mensajes: action.payload
+            }
+
+        case types.logout:
+            return {
+                uid: '',
+                chatActivo: null,
+                usuarios: [], 
+                mensajes: [], 
+            }
+
+        default:
+            return state;
+    }
+
+}
